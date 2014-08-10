d31cf21bdda0dffc35f217febbf4db3cc64dc238
manmohanmandhana <manmohanmandhana000@gmail.com>
2014-08-10 07:47:09 +0530

    files

---
+++ healthCare.php
@@ -0,0 +1,60 @@
+<?php include 'header.php'; ?>
+<!-- ============================================================= MAIN ============================================================= -->
+		
+		<main>
+			
+			
+			<!-- ============================================================= SECTION – WHO WE ARE ============================================================= -->
+			
+			<section id="who-we-are" style="padding:0px">
+				<div class="container inner-md">
+					<div class="row inner-top-xs">
+						
+						<div class="col-sm-offset-1 col-sm-11 inner-right-xs inner-bottom-xs">
+							
+							Healthcare Research Consortium and Technovation encourages UG and PG
+students to engage in developing HeathTech solutions, leading to
+development of products and processes addressing the healthcare needs of
+India.<br> Teams of students will work with faculty mentor(s); support will
+also be provided in terms of following:<br>
+
+   1.  <b>Funding:</b> Each project will be provided funding upto Rs. 1 lakh/year.<br>
+   2.  <b>Clinical Assistance:</b> The IITB Healthcare Research Consortium will
+facilitate by networking student teams with hospitals, clinicians &
+NGO's. Necessary clinical expertise/connectivity required will be
+provided.<br>
+   3.  <b>Workspace:</b> Teams requiring space will be provided access to the
+Biomedical Engg. teaching lab, BSBE (after class hours), Tinkerers'
+lab, and other prototyping facilities.<br>
+<br><br>
+We invite students to come up with their project proposals which will be
+selected after prior review and then the teams will start working under
+periodic performance review. We intend these to be a year-long projects
+and the proposal should contain these:<br>
+   -<b> Clinical need being addressed</b><br>
+   -<b> Specific objectives being targeted</b><br>
+   -<b> Novelty of the approach</b><br>
+   -<b> Clinical expertise needed</b>b ( to be made available through
+     Consortium partners)<br><br>
+
+More information about the Heathcare Research Consortium is available at:
+<a style="font-size:20px"href="http://healthcare-research.iitb.ac.in" target="_blank"><strong><u>Healthcare-research @iitb</u></strong></a>
+      <br><br>
+
+Student teams can send in their proposals to noronha@iitb.ac.in by  <b>24
+August 2014.</b><br><br>
+
+For any queries, please contact:<br>
+Prof. Santosh Noronha || noronha@iitb.ac.in || 7238<br>
+Amaldev V. || amaldev.000@gmail.com || 9820481964<br>
+
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</section>
+			
+			<!-- ============================================================= SECTION – WHO WE ARE : END ============================================================= -->
+			
+			
+			
+		<!-- ============================================================= MAIN : END ============================================================= -->
+		<?php include 'footer.php'; ?>
